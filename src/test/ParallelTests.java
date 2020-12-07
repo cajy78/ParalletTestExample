8ccd150c96c69917f6ecdeac06d1eae2a0cbc5f2
Cajetan <cajy78@live.com>
2020-12-07 20:19:06 +0530

    Initial commit

---
+++ src/test/ParallelTests.java
@@ -0,0 +1,77 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ParallelTests
+{
+	private WebDriver driver;
+	
+	@Test(groups = "Chrome")
+	public void launchChrome()
+	{
+		System.setProperty("webdriver.chrome.driver","/Users/cajetanfernandes/OneDrive/Documents/Studies and Certs/Automation Testing Masters/Phase 2/Jars/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		try
+		{
+			Thread.sleep(3000);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@Test(groups = "Chrome", dependsOnMethods = "launchChrome")
+	public void actionFBONChrome()
+	{
+		driver.findElement(By.name("login")).click();
+		try
+		{
+			Thread.sleep(3000);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@Test(groups = "Firefox")
+	public void launchFirefox()
+	{
+
+		System.setProperty("webdriver.gecko.driver","/Users/cajetanfernandes/OneDrive/Documents/Studies and Certs/Automation Testing Masters/Phase 2/Jars/geckodriver");
+		driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+		driver.manage().window().maximize();
+		try
+		{
+			Thread.sleep(3000);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		driver.close();
+	}
+	
+	@Test(groups = "Firefox", dependsOnMethods = "launchFirefox")
+	public void actionFBONFirefox()
+	{
+		driver.findElement(By.name("login")).click();
+		try
+		{
+			Thread.sleep(3000);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		driver.close();
+	}
+}
